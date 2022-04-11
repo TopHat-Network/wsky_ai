417111aa9aa8896690905490bac115e68ee83ce2
WhiskeeDev <email@liamholmes.co.uk>
2022-04-11 15:34:22 +0100

    adjust based on TS mods

--- scripts/client/taxi.js
+++ scripts/client/taxi.js
@@ -12,13 +12,14 @@ const maxSpeed = 22;
 
 RegisterCommand('taxi', async () => {
   let estimatedArrivalInMinutes = 0;
+
   const player = PlayerPedId();
   if (!player) {
     errorMsg('Player not found');
     return;
   } // player not found
 
-  const [x, y, z] = GetEntityCoords(player);
+  const [x, y, z] = GetEntityCoords(player, true);
 
   const waypointBlip = GetFirstBlipInfoId(8);
   if (!waypointBlip) {
@@ -26,26 +27,20 @@ RegisterCommand('taxi', async () => {
     return;
   } // no waypoint found
 
-  const waypointCoords = GetBlipInfoIdCoord(waypointBlip);
-  const [waypointX, waypointY] = waypointCoords;
+  const [waypointX, waypointY] = GetBlipInfoIdCoord(waypointBlip);
   const waypointZ = GetHeightmapBottomZForPosition(waypointX, waypointY);
 
-  const { vehicle, driver } = await generateTaxiandDriver({ x, y, z, heading: GetEntityHeading(player) });
+  const [vehicle, driver] = await generateVehicle('TAXI', [x, y, z], 0, 'a_m_m_fatlatin_01');
 
-  const maxNumberOfSeats = GetVehicleModelNumberOfSeats(vehicleHash);
-  const playerSeat = maxNumberOfSeats - 2;
+  const playerSeat = GetVehicleModelNumberOfSeats(vehicleHash) - 2; // Get the last available seat, minus the driver seat.
 
   SetPedIntoVehicle(player, vehicle, playerSeat);
-  TaskVehicleDriveToCoordLongrange(driver, vehicle, waypointX, waypointY, waypointZ, maxSpeed, drivingStyle, 0);
+  TaskVehicleDriveToCoordLongrange(driver, vehicle, waypointX, waypointY, waypointZ, maximumSpeed, drivingStyle, 0);
 
-  setTimeout(() => {
-    SetRadioToStationName('OFF'); // turn off radio, no way to control right now so better to just turn it off
-  }, 5000);
+  while (!hasVehicleArrivedAtDestination(vehicle, [waypointX, waypointY, waypointZ]) && IsPedSittingInVehicle(player, vehicle) && IsPedSittingInVehicle(driver, vehicle)) {
 
-  while (!hasVehicleArrivedAtDestination(vehicle, [waypointX, waypointY, waypointZ]) && IsPedSittingInVehicle(player, vehicle)) {
-
-    const vehicleSpeed = Math.max(maxSpeed * 0.5, GetEntitySpeed(vehicle)); // meters per second
-    const [vehicleX, vehicleY, vehicleZ] = GetEntityCoords(vehicle);
+    const vehicleSpeed = Math.max(maximumSpeed * 0.5, GetEntitySpeed(vehicle)); // meters per second
+    const [vehicleX, vehicleY, vehicleZ] = GetEntityCoords(vehicle, true);
     const distanceRemaining = CalculateTravelDistanceBetweenPoints(waypointX, waypointY, waypointZ, vehicleX, vehicleY, vehicleZ); // meters
 
     const timeRemainingInSeconds = (distanceRemaining / vehicleSpeed);
@@ -69,25 +64,24 @@ RegisterCommand('taxi', async () => {
   }
 
   const location = getLocationFromCoords(waypointX, waypointY, waypointZ);
-
   successMsg(`You have arrived at ${location}.`);
 
-  const vehicleHeading = GetEntityHeading(vehicle);
-
   console.log('parking vehicle');
-
+  const vehicleHeading = GetEntityHeading(vehicle);
   TaskVehiclePark(driver, vehicle, waypointX, waypointY, waypointZ, vehicleHeading, 0, 20, true);
 
-  while (!IsVehicleStopped(vehicle)) await Delay(50);
+  while (!IsVehicleStopped(vehicle)) {
+    await Delay(50);
+  }
 
   console.log('Player leaving vehicle');
-
   TaskLeaveVehicle(player, vehicle, 0);
 
-  while (!IsPedSittingInVehicle(player, vehicle)) await Delay(500);
+  while (!IsPedSittingInVehicle(player, vehicle)) {
+    await Delay(50);
+  }
 
   console.log('Setting Vehicle and Driver as no longer needed');
-
   SetVehicleAsNoLongerNeeded(vehicle);
   SetPedAsNoLongerNeeded(driver);
-})
+}, false)
